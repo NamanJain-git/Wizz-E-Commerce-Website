b110b26b7f46065160e31871a7569336b016ecaa
NamanJain-git <socialjainnaman@gmail.com>
2025-11-30 17:05:42 +0530

    beauty section update

--- src/Beauty/Beauty.jsx
+++ src/Beauty/Beauty.jsx
@@ -6,6 +6,11 @@ import womenimg1 from "../assets/productimgs/womenproduct1.webp"
 import womenimg2 from "../assets/productimgs/womenproduct11.jpeg"
 import womenimg3 from "../assets/productimgs/womenproduct9.jpeg"
 import womenimg4 from "../assets/productimgs/womenproduct8.jpeg"
+import beautyproduct1 from "../assets/productimgs/beautyproduct1.jpeg"
+import beautyproduct2 from "../assets/productimgs/beautyproduct2.jpeg"
+import beautyproduct3 from "../assets/productimgs/beautyproduct3.jpeg"
+import beautyproduct4 from "../assets/productimgs/beautyproduct4.jpeg"
+import beautyproduct5 from "../assets/productimgs/beautyproduct5.jpeg"
 import hairclaws from "../assets/Productimgs/hairclaws.jpeg"
 import facewash from "../assets/Productimgs/facewash.jpeg"
 import shampoo from "../assets/Productimgs/shampoo.jpeg"
@@ -26,17 +31,17 @@ const Beauty = () => {
                         
                                     <div className="beauty_products_box mt-10">
                                         <div className="beauty_products">
-                                            <img src={shampoo} alt="" />
-                                            <img src={beautyimg1} alt="" />
-                                            <img src={beautyimg1} alt="" />
+                                            <img src={beautyproduct5} alt="" />
+                                            <img src={hairclaws} alt="" />
+                                            <img src={beautyproduct4} alt="" />
                                             <img src={facewash} alt="" />
                                         </div>
                         
                                         <div className="beauty_products">
-                                            <img src={beautyimg1} alt="" />
+                                            <img src={beautyproduct2} alt="" />
                                             <img src={moisturizer} alt="" />
-                                            <img src={hairclaws} alt="" />
-                                            <img src={beautyimg1} alt="" />
+                                            <img src={beautyproduct1} alt="" />
+                                            <img src={beautyproduct3} alt="" />
                                         </div>
                                     </div>
                         
@@ -50,7 +55,7 @@ const Beauty = () => {
                         </div>
 
                          <div className="beauty_products mt-20">
-                                            <img src={womenimg1} alt="" />
+                                            <img src={shampoo} alt="" />
                                             <img src={womenimg2} alt="" />
                                             <img src={womenimg3} alt="" />
                                             <img src={womenimg4} alt="" />
